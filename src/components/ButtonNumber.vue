35d21dcaf7f9601a252a137b2399e38474394c5d
David Canut <david.canut@scrm.eu>
2021-04-20 10:14:31 +0200

    prepare project

---
+++ src/components/ButtonNumber.vue
@@ -0,0 +1,16 @@
+<template>
+  button number
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ButtonNumber',
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/services/calculator.service.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export function calculatorService() {
+    console.log('service')
+}
